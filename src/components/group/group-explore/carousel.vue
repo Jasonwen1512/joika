f92dc9aca29f581e5bd9fe2c23627f908cee8e11
Jasonwen1512 <jasonwen1512@gmail.com>
2025-07-19 16:28:56 +0800

    嘗試修復carousel卡片閃爍 2

--- src/components/group/group-explore/carousel.vue
+++ src/components/group/group-explore/carousel.vue
@@ -11,23 +11,23 @@ const imgList = Array.from({ length: 12 }, (_, i) => {
 
 // gsap區域
 onMounted(() => {
-  // const cards = document.querySelectorAll("img.card");
+  const cards = document.querySelectorAll(".scene .carousel .card");
 
-  // const total = cards.length;
-  // console.log(total);
+  const total = cards.length;
+  console.log(total);
 
-  // const angle = 360 / total;
-  // const radius = 1200;
+  const angle = 360 / total;
+  const radius = window.innerWidth >= 1024 ? 1800 : 1200;
 
-  // cards.forEach((card, i) => {
-  //   gsap.set(card, {
-  //     rotationY: angle * i,
-  //     transformPerspective: 1000,
-  //     z: radius,
-  //     xPercent: -50,
-  //     yPercent: -50,
-  //   });
-  // });
+  cards.forEach((card, i) => {
+    const rotateY = angle * i;
+    gsap.set(card, {
+      transform: `rotateY(${rotateY}deg) translateZ(${radius}px)`,
+      transformOrigin: "center center",
+      xPercent: -50,
+      yPercent: -50,
+    });
+  });
 
   gsap.to(".carousel", {
     rotateY: "+=360",
@@ -44,22 +44,22 @@ onMounted(() => {
   <h1>大家都在揪，就差你一咖</h1>
   <div class="scene">
     <div class="carousel">
-      <!-- <div
+      <div
         class="card"
         v-for="(img, index) in imgList"
         :key="index"
         :style="`--i: ${index}`"
       >
         <img :src="img" :alt="`img-${index + 1}`" />
-      </div> -->
-      <img
+      </div>
+      <!-- <img
         class="card"
         :style="{ '--i': index }"
         v-for="(img, index) in imgList"
         :key="index"
         :src="img"
         :alt="`img-${index + 1}`"
-      />
+      /> -->
     </div>
   </div>
 </template>
@@ -74,39 +74,39 @@ onMounted(() => {
 .carousel {
   position: relative;
   width: 600px;
-  height: 600px;
+  height: 650px; // 650px
   transform-style: preserve-3d;
+  // will-change: transform;
 }
 
 .card {
   position: absolute;
   width: 600px;
-  height: 800px;
+  height: 800px; // 800px
   background: $tp;
   box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
-  top: 50%;
+  top: 40%;
   left: 50%;
   transform-style: preserve-3d;
-  vertical-align: middle;
-  object-fit: cover;
-  transform: translate(-50%, -50%) rotateY(calc(var(--i) * 30deg))
-    translateZ(1200px);
+  // will-change: transform;
+  // transform: translate(-50%, -50%) rotateY(calc(var(--i) * 30deg))
+  //   translateZ(1200px);
   @include desktop() {
     width: 800px;
-    height: 1000px;
-    transform: translate(-50%, -50%) rotateY(calc(var(--i) * 30deg))
-      translateZ(1800px);
+    height: 1000px; // 1000px
+    // transform: translate(-50%, -50%) rotateY(calc(var(--i) * 30deg))
+    //   translateZ(1800px);
   }
-  // img {
-  //   width: 600px;
-  //   height: 800px;
-  //   vertical-align: middle;
-  //   object-fit: cover;
-  //   @include desktop() {
-  //     width: 800px;
-  //     height: 1000px;
-  //   }
-  // }
+  img {
+    width: 600px;
+    height: 800px;
+    vertical-align: middle;
+    object-fit: cover;
+    @include desktop() {
+      width: 800px;
+      height: 1000px;
+    }
+  }
 }
 
 h1 {
