45c3f5ef668c269beaf562acc1f27939e133b315
cheiyu <minnie870404@gmail.com>
2025-07-21 09:56:31 +0800

    暫存目前修改

---
+++ src/components/ActivityCard.vue
@@ -0,0 +1,69 @@
+<script setup>
+import LikeButton from "./LikeButton.vue";
+import Button from "./Button.vue";
+import { FakeActivity } from "@/assets/data/fake_activity";
+import { ref } from "vue";
+defineProps({
+  item: Object,
+});
+
+const likeMap = ref({});
+
+const toggleLike = (id) => {
+  likeMap.value[id] = !likeMap.value[id];
+};
+const aloha = () => {
+  alert("我要跟團！");
+};
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in FakeActivity" :key="item.activity_id" v-if="item">
+      <div class="activity_card">
+        <div class="activity_img">
+          <img :src="item.img_url" :alt="item.title" />
+        </div>
+        <h4 class="activity_title">{{ item.title }}</h4>
+        <p class="activity_intro">{{ item.intro }}</p>
+        <div class="button">
+          <Button :onClick="aloha" theme="primary" size="md">我要跟團!</Button>
+          <LikeButton
+            :isActive="likeMap[item.activity_id]"
+            @click="toggleLike(item.activity_id)"
+          ></LikeButton>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+.activity_card {
+  max-width: 295px;
+  width: 100%;
+  border: red solid 2px;
+  display: flex;
+  flex-direction: column;
+}
+
+.activity_img {
+  border: blue solid 2px;
+  justify-content: center;
+  align-items: center;
+}
+
+.activity_title {
+  border: green solid 2px;
+}
+.activity_intro {
+  border: purple solid 2px;
+}
+
+.button {
+  border: yellow solid 2px;
+  display: flex;
+  justify-content: space-between;
+  padding: 5px 10px;
+}
+</style>
---
+++ src/components/LikeButton.vue
@@ -0,0 +1,46 @@
+<script setup>
+import HeartOutlineIcon from "@/assets/img/icon/heart_outline.svg";
+import HeartFilled from "@/assets/img/icon/heart_filled.svg";
+const props = defineProps({
+  icon: {
+    type: Object,
+    required: true,
+  },
+
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+  onClick: {
+    type: Function,
+  },
+});
+</script>
+<template>
+  <div class="button-wrapper">
+    <button class="icon-button" :data-is-active="isActive" @click="onClick">
+      <component :is="isActive ? HeartFilled : HeartOutlineIcon" class="icon" />
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.icon-button {
+  color: $red;
+  cursor: pointer;
+  padding: 5px;
+
+  &:hover {
+    color: $pink;
+  }
+
+  .icon {
+    width: 40px;
+    height: 40px;
+    fill: $red;
+  }
+}
+.icon-button[data-is-active="true"] .icon {
+  fill: red;
+}
+</style>
