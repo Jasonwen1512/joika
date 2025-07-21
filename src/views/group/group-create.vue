77f6748d6763fc5745f634961058a40499629c59
Jasonwen1512 <jasonwen1512@gmail.com>
2025-07-21 10:52:54 +0800

    完善_color.scss、按鈕樣式測試（在group/group-create内）

--- src/views/group/group-create.vue
+++ src/views/group/group-create.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import Test from "@/components/test.vue";
+</script>
 
 <template>
   <!-- 這是發起揪團頁 -->
@@ -12,6 +14,7 @@
   <div>這是發起揪團頁</div>
   <div>這是發起揪團頁</div>
   <div>這是發起揪團頁</div>
+  <Test />
 </template>
 
 <style scoped lang="scss"></style>
