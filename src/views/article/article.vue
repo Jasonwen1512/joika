fa895db8d15262ad1cad54282bf324b3c0b45b08
zzjhuang <miro37091@gmail.com>
2025-07-23 16:22:09 +0800

    更新分類欄

--- src/views/article/article.vue
+++ src/views/article/article.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Button from '@/components/Button.vue'
 const activities = [
   { class: 'camping', label: '露營', color: '#FFB6C1' },
   { class: 'boardGames', label: '桌遊', color: '#ADD8E6' },
@@ -6,6 +7,15 @@ const activities = [
   { class: 'movie', label: '電影', color: '#FFD700' },
   { class: 'dinnerGathering', label: '聚餐', color: '#FFA07A' }
 ]
+import { ref } from 'vue'
+
+const categories = ['全部', '潛水', '球類', '登山', '露營', '電影', '桌遊', '其他']
+const activeCategory = ref('全部')
+
+function setActive(category) {
+  activeCategory.value = category
+}
+
 </script>
 
 <template>
@@ -24,6 +34,28 @@ const activities = [
       </div>
     </div>
   </div>
+        <main class="artic">
+    <div class="title">
+      <h2>文章列表</h2>
+    </div>
+
+    <section class="articleCategory">
+      <div class="categoryList">
+        <div
+          v-for="(cat, index) in categories"
+          :key="index"
+          :class="['categoryLabel', { active: cat === activeCategory }]"
+          @click="setActive(cat)"
+        >
+          {{ cat }}
+        </div>
+      </div>
+
+     <Button :onClick="ula" theme="primary" size="md">我要發文</Button>
+    </section>
+
+    <hr />
+  </main>
 </template>
 
 <style scoped lang="scss">
@@ -32,13 +64,15 @@ const activities = [
 }
 
 .imglist {
-  display: flex;
-  gap: 5vw;
+display: flex;
+    gap: 1vw;
+    margin: 4vw 2vw;
+    height: 30vh;
 }
 
 .activity {
   flex: 1;
-  height: 100px;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -46,5 +80,41 @@ const activities = [
   font-weight: bold;
   border-radius: 10px;
   text-align: center;
+}
+
+
+.categoryLabel {
+    padding: 15px 25px;
+    background-color: #81bfda;
+    border: 1px black solid;
+}
+
+.active{
+  background-color: #4F8DA8;
+color: #fff;
+}
+  .categoryList {
+ display: flex;
+ gap: 18px;
+}
+ .title {
+ margin-block: 3vh;
+ }
+
+section.articleCategory {
+display: flex;
+justify-content: space-between;
+max-width: 1200px;
+width: 100%;
+align-items: center;
+ }
+
+main.artic {
+    max-width: 1200px;
+    align-content: center;
+    margin: auto;
+}
+hr {
+    margin-block: 5vh;
 }
 </style>
