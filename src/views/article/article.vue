976918d11fd3cf33ef2aca2e9742879d64c060d3
Jasonwen1512 <jasonwen1512@gmail.com>
2025-07-23 09:47:36 +0800

    重新npm i

--- src/views/article/article.vue
+++ src/views/article/article.vue
@@ -4,9 +4,8 @@
   <!-- 這是熱門文章頁 -->
   <div>這是熱門文章頁</div>
   <div class="banner">
-    <img src ="assets/Image/文章分類banner.jpg">
+    <!-- <img src ="assets/Image/文章分類banner.jpg"> -->
   </div>
-  
 </template>
 
 <style scoped lang="scss"></style>
