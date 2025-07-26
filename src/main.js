ca66a9cb5e3a69921c490ab72f21e8037ab1818e
chi-yh <chi802827@gmail.com>
2025-07-26 18:47:49 +0800

    新增首頁：分類輪播、品牌介紹、背景圖片

--- src/main.js
+++ src/main.js
@@ -11,10 +11,9 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { faHeart as fasHeart } from "@fortawesome/free-solid-svg-icons";
-import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
+
 /* add icons to the library */
-library.add(fasHeart, farHeart);
+
 
 const app = createApp(App);
 app.component("font-awesome-icon", FontAwesomeIcon);
