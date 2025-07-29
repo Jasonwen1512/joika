3fbb718b8da76448ee820c9296ccb20caac74f30
chi-yh <chi802827@gmail.com>
2025-07-29 12:47:48 +0800

    vite.config.js更新完成，本機端問題解決

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,43 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig ,loadEnv} from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import { resolve } from "path";
+import svgLoader from "vite-svg-loader";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode }) =>{
   const env = loadEnv(mode, process.cwd())
-  return {
-    plugins: [vue(), vueDevTools()],
-    base: env.VITE_BASE || '/',
-    build: {
-      outDir: env.VITE_OUT_DIR || 'dist',
-    },
-
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `
-            @use "@/assets/styles/page/index.scss" as *;
-          `,
+  return{
+    plugins: [
+      vue(),
+      svgLoader({
+        svgoConfig: {
+          plugins: [
+            {
+              name: "cleanupIds",
+              params: {
+                remove: false, // 不移除 ID
+                minify: false, // 不縮短 ID 名稱
+              },
+            },
+          ],
         },
-      },
+      }),
+      vueDevTools(),
+    ],
+      base: env.VITE_BASE || '/',
+      build: {
+      outDir: env.VITE_OUT_DIR || 'dist'
     },
     resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
-    },
+          alias: { "@": resolve(__dirname, "src") },
+        },
+        css: {
+          preprocessorOptions: {
+            scss: {
+              additionalData: `@use "@/style.scss" as *;`,
+            },
+          },
+        },
   }
 })
