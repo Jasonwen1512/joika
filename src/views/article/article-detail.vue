b253984a4f04517c72980d695bbfe5400f8a0730
zzjhuang <miro37091@gmail.com>
2025-08-01 01:25:08 +0800

    1.文章內頁初步切版 2.假資料分類錯誤更正

--- src/views/article/article-detail.vue
+++ src/views/article/article-detail.vue
@@ -10,19 +10,41 @@ const route = useRoute()
 const postid = route.params.postid
 const article = articleList.find(item => item.postid === postid)
 
+
+//分類顏色
+  const EventColorMap = {
+  "登山": "#56DD61",
+  "水上活動": "#81BFDA",
+  "運動": "#567ADD",
+  "露營": "#F05ECC",
+  "唱歌": "#FADA7A",
+  "展覽": "#FF7F5B",
+  "聚餐": "#FFB65D",
+  "桌遊": "#F5F0CD",
+  "電影": "#B1F0F7",
+  "手作": "#DBF964",
+  "文化體驗": "#6AF6C5",
+  "演出表演": "#EA64FF",
+  "其他": "#969696"
+};
+const GetEventColor = (eventName) => {
+  return EventColorMap[eventName] || '#adb5bd';
+};
 </script>
 
 <template>
-  <background>
+  <div>
   <!-- <img class="bg-img2" src="/src/assets/img/bg-decorate2.png" alt="背景圖藍">
   <img class="bg-img3" src="/src/assets/img/bg-decorate3.png" alt="背景圖黃"> -->
-</background>
+  </div>
 
 <main>
   <section class="author-information">
     <!-- 這邊之後要串會員資料頭像與名稱  先暫時寫死 -->
-<div class="avatar"><img src="/src/assets/img/bg-decorate3.png" class="masked-avatar" />    
-  <p>文章作者：{{ article.userid }}</p>
+<div class="avatar">
+  <img class="avatar-img" src="/src/assets/img/member/headshot.jpg" alt="Member Headshot">
+  
+  <p>{{ article.userid }}</p>
 </div>
     <div class="btn-list">
   <Button :suffixIcon="SmEditIcon" theme="info" size="sm">編輯</Button>  
@@ -30,19 +52,20 @@ const article = articleList.find(item => item.postid === postid)
     </div>
   </section>
   <section class="Content">
-  <div class="article" v-if="article">
-
-
-
-    <h3>{{ article.postid }}</h3>
-            <div><span>分類：{{ article.event }}</span>
-發布時間：{{ article.date }}</div>
-<div class="article-img"> {{article.image }}</div>
-    <p v-html="article.content"></p>
-  </div>
-  <div v-else>
-    <p>找不到這篇文章。</p>
-  </div>
+    <div class="article" v-if="article">
+      <h3>{{ article.title }}</h3>
+      <div class="article-head">   
+        <span  class="event-label"  :style="{ borderColor: GetEventColor(article.event), }">{{ article.event }}</span> 
+        <p>{{ article.date }}</p>
+      </div>
+      <div class="article-img">
+  <img :src="article.image" alt="文章圖片" />
+</div>
+      <p v-html="article.content" alt="內文"></p>
+    </div>
+    <div v-else>
+      <p>找不到這篇文章。</p>
+    </div>
 </section>
 <section class="Comment">
   <!-- 這邊也要串 先寫死 -->
@@ -58,6 +81,7 @@ const article = articleList.find(item => item.postid === postid)
 body{
   position: relative;
 }
+
 .bg-img3{
     position: absolute;
     bottom: -50vh;
@@ -73,22 +97,67 @@ body{
     width: 15%;
     z-index: -999;
 }
-
-.article{
-
-max-width: 1200px;
-display: block;
-margin: auto;
-
+main{
+  max-width: 1200px;
+  margin:auto;
+  padding: 20px;
 }
 
-  .masked-avatar {
-    width: 50px;
-    height: 50px;
-    mask-image: radial-gradient(circle, black 98%, transparent 100%);
-    -webkit-mask-image: radial-gradient(circle, black 98%, transparent 100%);
-    mask-size: cover;
-    mask-repeat: no-repeat;
-  }
+.author-information {
+    display: flex
+;
+    justify-items: center;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-content: flex-end;
+    justify-content: space-between;
+    align-items: center;
+}
 
+
+.avatar{
+    display: flex;
+    gap: 10px;
+    align-items: center;
+}
+
+.avatar-img {
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  object-fit: cover;
+  display: block;
+}
+.btn-list{
+  display: flex;
+gap: 10px;
+}
+
+.article{
+max-width: 1200px;
+margin: 10px auto;
+display: flex;
+  flex-direction: column; 
+  gap: 10px;
+
+}
+.article-head {
+    display: flex;
+    align-items: center;
+    padding-block: 5px;
+}
+.event-label {
+    text-align: center;
+    padding: 5px;
+    margin: 5px;
+    border: solid 3px;
+    border-radius: 6px;
+    width: 80px;
+}
+.article-img img {
+  width: 100%;
+  height: auto;
+  display: block;
+  border-radius: 8px;
+}
 </style>
