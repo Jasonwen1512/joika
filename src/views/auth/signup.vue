128d130258bf2430f261c8d6d806f71eee620827
Amooo1122 <enli841122@gmail.com>
2025-08-03 16:27:04 +0800

    註冊修改

--- src/views/auth/signup.vue
+++ src/views/auth/signup.vue
@@ -1,38 +1,122 @@
 <script setup>
-import { ref } from "vue";
-import Button from "@/components/Button.vue";
+  import { ref } from "vue";
+  import Button from "@/components/Button.vue";
+  import InputField from '@/components/auth/Inputfield.vue'
 
-const goNext = () => {
-  if (currentStep.value < 3) {
-    currentStep.value++;
-    console.log(`Current Step: ${currentStep.value}`);
+  const currentStep = ref(1);
+  const avatarUrl = ref('');
+
+  const stepOne = ref({
+    email: '',
+    phone: '',
+    password: '',
+    confirmPassword: '',
+    agreed: false,
+  });
+
+  const errors = ref({
+    email: '',
+    phone: '',
+    password: '',
+    confirmPassword: '',
+    agreed: '',
+  });
+
+const validateStepOne = () => {
+  // 清除錯誤
+  errors.value = {
+    email: '',
+    phone: '',
+    password: '',
+    confirmPassword: '',
+    agreed: '',
+  };
+
+  const { email, phone, password, confirmPassword, agreed } = stepOne.value;
+  let hasError = false;
+
+  if (!email) {
+    errors.value.email = '請輸入信箱';
+    hasError = true;
+  }else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    errors.value.email = '信箱格式錯誤';
+    hasError = true;
+  }
+
+  if (!phone) {
+    errors.value.phone = '請輸入手機';
+    hasError = true;
+  }else if (!/^09\d{8}$/.test(phone)) {
+    errors.value.phone = '手機格式錯誤';
+    hasError = true;
+  }
+
+  if (!password) {
+    errors.value.password = '請輸入密碼';
+    hasError = true;
+  }
+
+  if (!confirmPassword) {
+    errors.value.confirmPassword = '請確認密碼';
+    hasError = true;
+  } else if (password !== confirmPassword) {
+    errors.value.confirmPassword = '密碼不一致';
+    hasError = true;
+  }
+
+  // if (!agreed) {
+  //   errors.value.agreed = '請勾選會員條款';
+  //   hasError = true;
+  // }
+
+  if (!hasError) {
+    goNext();
+  }
+};
+
+
+  const goNext = () => {
+    if (currentStep.value < 3) {
+      currentStep.value++;
+      console.log(`Current Step: ${currentStep.value}`);
+    }
+  };
+
+  const getStepState = (step) => {
+    if (currentStep.value > step) return 'completed';
+    if (currentStep.value === step) return 'current';
+    return '';
+  };
+
+  const handleAvatarChange = (e) => {
+  const file = e.target.files[0];
+  if (file) {
+    avatarUrl.value = URL.createObjectURL(file);
   }
 };
 </script>
 
 <template>
-  <div class="registration">
-    <div class="registration-container">
+  <div class="signup-page">
+    <div class="signup-container">
       <div class="registration-header">
         <h1>註冊</h1>
         <ul class="progress-bar" id="progress-bar">
           <li
-            :class="['progress-step', currentStep >= 1 ? 'active' : '']"
+            :class="['progress-step', getStepState(1)]"
             data-step="1"
           >
             <div class="step-label">帳號申請</div>
             <div class="step-circle"></div>
           </li>
           <li
-            :class="['progress-step', currentStep >= 2 ? 'active' : '']"
-            data-step="2"
+            :class="['progress-step', getStepState(2)]"
           >
             <div class="step-label">基本資料</div>
             <div class="step-circle"></div>
           </li>
           <li
-            :class="['progress-step', currentStep === 3 ? 'active' : '']"
-            data-step="3"
+            :class="['progress-step', getStepState(3)]"
           >
             <div class="step-label">註冊完成</div>
             <div class="step-circle"></div>
@@ -41,48 +125,61 @@ const goNext = () => {
       </div>
 
       <!--步驟1-->
-      <section v-if="currentStep === 1" class="form-step active">
-        <form>
-          <div class="form-group">
-            <label for="email">信箱</label>
-            <input type="email" id="email" name="email" required />
-          </div>
-          <div class="form-group">
-            <label for="phone">手機</label>
-            <input type="tel" id="phone" name="phone" required />
-          </div>
-          <div class="form-group">
-            <label for="password">密碼</label>
-            <input type="password" id="password" name="password" required />
-          </div>
-          <div class="form-group">
-            <label for="confirm-password">確認密碼</label>
-            <input
-              type="password"
-              id="confirm-password"
-              name="confirm-password"
-              required
-            />
-          </div>
+      <section v-show="currentStep === 1" class="form-step">
+        <form @submit.prevent="validateStepOne">
+          <InputField
+            id="email"
+            label="信箱"
+            type="email"
+            v-model="stepOne.email"
+            :error="errors.email"
+          />
+
+          <InputField
+            id="phone"
+            label="手機"
+            type="tel"
+            v-model="stepOne.phone"
+            :error="errors.phone"
+          />
+
+          <InputField
+            id="password"
+            label="密碼"
+            type="password"
+            v-model="stepOne.password"
+            :error="errors.password"
+          />
+
+          <InputField
+            id="confirm-password"
+            label="確認密碼"
+            type="password"
+            v-model="stepOne.confirmPassword"
+            :error="errors.confirmPassword"
+          />
           <div class="tos-group">
-            <input type="checkbox" id="tos" name="tos" required />
+            <input type="checkbox" id="tos" v-model="stepOne.agreed" />
             <label for="tos">我已閱讀並同意 JOIKA 會員條款</label>
           </div>
 
           <div class="button-group">
-            <Button size="md" theme="primary" @click="goNext">送出</Button>
+            <Button size="md" theme="primary">送出</Button>
           </div>
         </form>
       </section>
 
       <!-- 步驟 2-->
-      <section v-if="currentStep === 2" class="form-step">
-        <form>
+      <section v-show="currentStep === 2" class="form-step">
+        <form >
           <div class="avatar-upload">
-            <label for="avatar-input">
-              <div class="avatar-circle">頭貼上傳</div>
+            <label for="avatar-input" class="avatar-label">
+              <div class="avatar-circle">
+                <img v-if="avatarUrl" :src="avatarUrl" alt="預覽頭貼" />
+                <span v-else>頭貼上傳</span>
+              </div>
             </label>
-            <input type="file" id="avatar-input" accept="image/*" />
+            <input type="file" id="avatar-input" accept="image/*" @change="handleAvatarChange" hidden />
           </div>
           <div class="form-group">
             <label for="name">姓名</label>
@@ -125,8 +222,8 @@ const goNext = () => {
       </section>
 
       <!-- 步驟 3: 註冊完成 -->
-      <section v-if="currentStep === 3" class="form-step">
-        <h2>註冊成功</h2>
+      <section v-show="currentStep === 3" class="form-step">
+        <p class="subtitle">註冊成功</p>
         <p class="subtitle">就差一點點就能一起出發啦！</p>
         <p class="info">我們正在確認您的資料，審核通過後就可以開始揪團玩樂囉</p>
       </section>
@@ -141,18 +238,45 @@ const goNext = () => {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+  // border: 1px solid red;
 }
 
-.registration {
+.signup-page {
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 50px 0;
   padding: 20px;
   position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    background-image: url("@/assets/img/bg-decorate5.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    width: 150px;
+    aspect-ratio: 334 / 591;
+    top: -20px;
+    left: 0px;
+    z-index: -1;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    background-image: url("@/assets/img/bg-decorate7.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    aspect-ratio: 406 / 518;
+    width: 150px;
+    bottom: -50px;
+    right:0;
+    z-index: -1;
+  }
 }
 
-.registration-container {
+.signup-container {
   width: 100%;
   max-width: 800px;
   background-color: $white;
@@ -163,97 +287,13 @@ const goNext = () => {
   h1 {
     text-align: center;
     font-size: $font-size-h3;
-    margin-bottom: 30px;
-  }
-
-  .registration-header {
-    align-items: center;
-    margin-bottom: 30px;
-  }
-
-  .progress-bar {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    list-style: none;
-    position: relative;
-    margin: 0 auto;
-    padding: 0 10px;
-  }
-
-  .progress-step {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    .step-circle {
-      width: 25px;
-      height: 25px;
-      border-radius: 50%;
-      background-color: $white;
-      border: 1px solid $black;
-      position: relative;
-      display: inline-block;
-      transition: all 0.3s ease;
-
-      &.active {
-        background-color: $blue;
-      }
-
-      &::after {
-        content: "";
-        position: absolute;
-        top: 50%;
-        left: 100%;
-        width: 100px;
-        height: 2px;
-        background-color: $black;
-        transform: translateY(-50%);
-      }
-    }
-
-    &:last-child .step-circle::after {
-      display: none;
-    }
+    font-weight: 400;
+    margin-bottom: 40px;
   }
 
   .step-label {
     font-size: 14px;
     margin-bottom: 5px;
-  }
-
-  .form-step {
-    display: none;
-  }
-  .form-step.active {
-    display: block;
-  }
-
-  .form-group {
-    margin-bottom: 30px;
-  }
-
-  label {
-    display: block;
-    margin-bottom: 8px;
-  }
-
-  input[type="email"],
-  input[type="text"],
-  input[type="password"],
-  input[type="tel"] {
-    width: 100%;
-    padding: 12px 15px;
-    border: 1px solid $black;
-    border-radius: 3px;
-    transition: border-color 0.2s, box-shadow 0.2s;
-
-    &:focus {
-      outline: none;
-      border-color: $color-primary;
-      box-shadow: 0 0 0 2px rgba($color-primary, 0.3);
-    }
   }
 
   .tos-group {
@@ -274,5 +314,147 @@ const goNext = () => {
     display: flex;
     justify-content: center;
   }
+
+  .subtitle{
+    font-size:$font-size-h3;
+    color:$blue ;
+    text-align: center;
+    margin-bottom: 30px;
+  }
+
+  .info{
+    text-align: center;
+    color:$black;
+    margin-bottom: 30px;
+
+  }
 }
+
+.registration-header {
+  align-items: center;
+  margin-bottom: 30px;
+    
+  .progress-bar {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    max-width: 420px;
+    align-items: center;
+    list-style: none;
+    margin: 0 auto;
+    padding: 0 10px;
+  }
+
+  .progress-step {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .step-circle {
+      width: 25px;
+      height: 25px;
+      border-radius: 50%;
+      background-color: $white;
+      border: 1px solid $black;
+      position: relative;
+      display: inline-block;
+      transition: all 0.3s ease;
+
+      &::after {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 100%;
+        width: 110px;
+        height: 2px;
+        background-color: $black;
+        transform: translateY(-50%);
+      }
+    }
+    
+    &:last-child .step-circle::after {
+      display: none;
+    }
+
+    &.completed .step-circle {
+      background-color: #C6F1FB; // 淺藍色（已完成）
+    }
+
+    &.current .step-circle {
+      background-color: #4A87A7; // 深藍色（當下步驟）
+    }
+  }
+}
+
+
+.avatar-upload {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+
+  .avatar-label {
+    cursor: pointer;
+  }
+
+  .avatar-circle {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    background-color: #ddd;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    font-size: 14px;
+    color: #666;
+    transition: 0.3s;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+}
+
+
+@media (min-width: 768px) {
+  .signup-container {
+    border-radius: 6px;
+    padding: 50px 100px;
+
+    h1 {
+      font-size: $font-size-h2;
+      font-weight: 600;
+      margin-bottom: 30px;
+    }
+
+  }
+
+  .registration-header {
+    .progress-step {
+      .step-circle {
+        &::after {
+          width: 150px;
+        }
+      }
+    }
+  }
+
+    .subtitle{
+      font-size:$font-size-h2;
+      margin-bottom: 50px;
+    }
+
+    .info{
+      text-align: center;
+      color:$black;
+      margin-bottom: 50px;
+    }
+  }
 </style>
