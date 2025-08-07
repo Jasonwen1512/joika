7050fe666627d3cabb235804e3bcad5fe0820094
zzjhuang <miro37091@gmail.com>
2025-08-08 02:12:39 +0800

    1新增團員列表頁(還沒R完)2.更新router 新增member-list 3.更新member活動卡的連結

--- src/components/member/member-activity-card.vue
+++ src/components/member/member-activity-card.vue
@@ -1,45 +1,57 @@
-  <script setup>
-  import ActivityTag from "@/components/member/activity-status-tag.vue";
-  import Button from "@/components/Button.vue";
-  const props = defineProps({
+<script setup>
+import ActivityTag from "@/components/member/activity-status-tag.vue";
+import Button from "@/components/Button.vue";
+const props = defineProps({
     item: Object,
     status: String,
-  });
-  </script>
+});
+</script>
 
-  <template>
-    <div v-if="item" class="member-activity-card">
-      <div class="img">
-        <img :src="item.activity_img" :alt="item.activity_name" />
-        <ActivityTag :activity_status="item.activity_status" />
-      </div>
-      <h3 class="activity-name">{{ item.activity_name }}</h3>
-      <p class="activity-description">{{ item.activity_description }}</p>
-      <div class="button-group">
-        <Button theme="info" size="sm">團員列表</Button>
-      </div>
+<template>
+    <div
+        v-if="item"
+        class="member-activity-card"
+    >
+        <div class="img">
+            <img
+                :src="item.activity_img"
+                :alt="item.activity_name"
+            />
+            <ActivityTag :activity_status="item.activity_status" />
+        </div>
+        <h3 class="activity-name">{{ item.activity_name }}</h3>
+        <p class="activity-description">{{ item.activity_description }}</p>
+        <div class="button-group">
+            <RouterLink :to="'/member/member-list'">
+                <Button
+                    theme="info"
+                    size="sm"
+                    >團員列表</Button
+                ></RouterLink
+            >
+        </div>
     </div>
-  </template>
+</template>
 
-  <style lang="scss" scoped>
-  .member-activity-card {
+<style lang="scss" scoped>
+.member-activity-card {
     width: 284px;
     height: 390px;
     justify-self: center;
     gap: 25px;
     margin: 20px;
-  }
-  .img {
+}
+.img {
     width: 264px;
     height: 200px;
     position: relative;
-  }
-  .img img {
+}
+.img img {
     width: 100%;
     height: 100%;
-  }
+}
 
-  .activity-name {
+.activity-name {
     max-width: 264px;
     font-size: $font-size-h4;
     padding: 14px 0 0px;
@@ -49,9 +61,9 @@
     text-overflow: ellipsis;
     font-weight: unset;
     letter-spacing: 0.5px;
-  }
+}
 
-  .activity-description {
+.activity-description {
     color: $black;
     font-size: $font-size-p;
     padding: 10px 0 0;
@@ -61,13 +73,13 @@
     -webkit-box-orient: vertical;
     -webkit-line-clamp: 3;
     overflow: hidden;
-  }
+}
 
-  .button-group {
+.button-group {
     display: flex;
     justify-content: right;
 
     padding-top: 10px;
     max-width: 264px;
-  }
-  </style>
+}
+</style>
