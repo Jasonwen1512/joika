33e06e21e08b3f7a57c4fdbd2eb6029fd9c015ba
DORIS\minni <minnie870404@gmail.com>
2025-08-12 21:11:57 +0800

    feat: group-create.vue 修改表單為element-plus         group-create-popup.vue 移除監聽 (待測試)         group-create-preview.vue 新增表單預覽(電腦版尚未排版)
    data:group-form.js 新增表單pinia store

--- src/router/index.js
+++ src/router/index.js
@@ -78,7 +78,10 @@ const routes = [
   path: '/member/member-list',
   component: () => import('@/views/member/member-list.vue')
 },
-
+  {
+    path: "/group/form-preview",
+    component: () => import("@/views/group/group-create-preview.vue"),
+  },
   {
     path: "/:pathMatch(.*)*",
     name: "404",
